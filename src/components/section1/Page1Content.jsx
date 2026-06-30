e36127f06fc6b1cbfe8a93277c276a8585a14409
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-06-30 14:51:50 +0530

    Priject 2

---
+++ src/components/section1/Page1Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LeftContent from './LeftContent'
+import RightContent from './RightContent'
+
+const Page1Content = () => {
+  return (
+    <div className='px-18 py-3 bg-amber-950 h-[90vh]'>
+        Page1Content
+        <LeftContent />
+        <RightContent />
+    </div>
+  )
+}
+
+export default Page1Content
